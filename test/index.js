aee6587141100485519f4ebb5e1a9f31b4961afe
Tom Chen <developer@tomchentw.com>
2014-11-07 09:20:20 +0800

    chore(test): port from __template__gist__

--- test/index.js
+++ test/index.js
@@ -1,1 +1,8 @@
-require("../lib")(["./lib"]);
+"use strict";
+/**
+ * __template__gist__: https://gist.github.com/tomchentw/0dc24c30955c1a6c94d4#file-test__index-js
+ */
+describe("mocha-jscs", function() {
+  require("mocha-jshint")(["./lib/"]);
+  require("../lib")(["./lib/"]);
+});
