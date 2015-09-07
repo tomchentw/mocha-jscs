d2624128dae786a8104b8a91c6566ac039bf7f26
Tom Chen <developer@tomchentw.com>
2015-09-07 21:53:10 +0800

    test(index): fix mocha-jshint breaking changes for upgrade

--- test/index.js
+++ test/index.js
@@ -3,7 +3,9 @@
  * __template__gist__: https://gist.github.com/tomchentw/0dc24c30955c1a6c94d4#file-test__index-js
  */
 describe("mocha-jscs", function() {
-  require("mocha-jshint")(["./lib/"]);
+  require("mocha-jshint")({
+  	paths: ["./lib/"],
+  });
   require("../lib")(["./lib/"]);
   require("../lib")(["./lib/index.js"]);
 });
