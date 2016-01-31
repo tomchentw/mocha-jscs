08bb1735e74ebbc3f1da7ac56c41c497ba949f60
Tom Chen <developer@tomchentw.com>
2016-01-31 13:54:09 +0800

    chore(*): remove __template__gist__

--- test/index.js
+++ test/index.js
@@ -1,7 +1,5 @@
 "use strict";
-/**
- * __template__gist__: https://gist.github.com/tomchentw/0dc24c30955c1a6c94d4#file-test__index-js
- */
+
 describe("mocha-jscs", function() {
   require("mocha-jshint")({
   	paths: ["./lib/"],
